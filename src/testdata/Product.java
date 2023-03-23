62a86b839dd9e06ad1b84be787a10768e3ecc992
cristianciula <cristianciula@gmail.com>
2023-03-23 12:07:24 +0200

    Added following clases and files:
    - Product class in testdata
    - product json file in testdatafiles
    - ProductsTests class in tests

---
+++ src/testdata/Product.java
@@ -0,0 +1,35 @@
+package testdata;
+
+import utils.Reader;
+
+public class Product {
+
+    private String title;
+    private String description;
+    private String price;
+
+    public Product(String fileName) {
+        this.title = Reader.json(fileName).get("title").toString();
+        this.description = Reader.json(fileName).get("description").toString();
+        this.price = Reader.json(fileName).get("price").toString();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getPrice() {
+        return price;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
---
+++ src/tests/ProductsTests.java
@@ -0,0 +1,24 @@
+package tests;
+
+import messages.ProductsMessages;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ProductsTests extends BaseTest{
+
+    @BeforeEach
+    public void beforeEach() {
+        super.beforeEach();
+
+        loginPage.authenticate(standardUser);
+        assertEquals(ProductsMessages.PRODUCTS_PAGE_TITLE, productsPage.getPageTitle());
+        assertTrue(header.shoppingCartIsDisplayed());
+    }
+
+    @Test
+    public void addProductToCart() {
+    }
+}
