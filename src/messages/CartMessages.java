4e56db77e988fe4b6bcb969bdc1a339749ba5bfd
cristianciula <cristianciula@gmail.com>
2023-03-26 22:00:36 +0300

    Cart page
    - Added CartPage class
    - Added CartMessages class

---
+++ src/messages/CartMessages.java
@@ -0,0 +1,6 @@
+package messages;
+
+public class CartMessages {
+
+    public final static String CART_PAGE_TITLE = "Your Cart";
+}
---
+++ src/pages/CartPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+    WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //LOCATORS
+
+
+    //ACTIONS
+}
