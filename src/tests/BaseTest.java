b7efd91fd582b6c063e3f7ed32087aef1450f4f2
cristianciula <cristianciula@gmail.com>
2023-03-30 17:59:42 +0300

    Updates

--- src/tests/BaseTest.java
+++ src/tests/BaseTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.CartPage;
 import pages.LoginPage;
 import pages.ProductDetailsPage;
